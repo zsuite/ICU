fc29a53a7728a26c50ec63e61e9aebfe00064161
Zach Suite <zachissuite@gmail.com>
2015-02-04 14:56:33 -0800

    Init Commit

---
+++ Assets/PFX/moveCar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCar : MonoBehaviour {
+
+	private float xPos;
+	private float yPos;
+	private float zPos;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		xPos = gameObject.transform.position.x;
+		yPos = gameObject.transform.position.y;
+		zPos = gameObject.transform.position.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xPos+= speed;
+
+		gameObject.transform.position = new Vector3(xPos, yPos, zPos);
+	}
+}
