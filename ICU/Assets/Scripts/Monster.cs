64fef97994497be989d5f8bd45ab634bf9f7da46
Zach Suite <zachissuite@gmail.com>
2015-02-14 00:25:29 -0800

    modified the rain

--- ICU/Assets/Scripts/Monster.cs
+++ ICU/Assets/Scripts/Monster.cs
@@ -7,13 +7,14 @@ public class Monster : MonoBehaviour {
 	public float soundEffectPitchRandomness = 0.5f;
 	public int stage;
 	public GameObject player;
+	public GameObject lookAtMe;
 	public GameObject blinkQuad;
 	Vector3 differenceBetween;
 	bool blinkOnce;
 	// Use this for initialization
 	void Start () {
 		blinkOnce = true;
-		blinkQuad.SetActive(false);
+		//blinkQuad.SetActive(false);
 
 		stage =1;
 		mon = new SightObject();
@@ -25,7 +26,10 @@ public class Monster : MonoBehaviour {
 	void Update () {
 		if(renderer.isVisible && EyeValues.playerBlinked && blinkOnce){
 			Teleport(stage);
-			blinkQuad.SetActive(true);
+			//blinkQuad.SetActive(true);
+			transform.LookAt(lookAtMe.transform.position);
+			
+			audio.Play();
 			audio.pitch = Random.Range(1.0f - soundEffectPitchRandomness, 1.0f + soundEffectPitchRandomness);
 
 			blinkOnce =false;
@@ -44,24 +48,18 @@ public class Monster : MonoBehaviour {
 		switch(_stage){
 		case 1:
 			this.gameObject.transform.position = transform.position + (player.transform.position - transform.position) * 0.5f;
-			transform.LookAt(player.transform.position);
 
-			audio.Play();
 			if(Vector3.Distance(this.gameObject.transform.position, player.transform.position) <= 5){
 				stage = 2;
 			}
 			break;
 		case 2:
 			this.gameObject.transform.position = transform.position + (player.transform.position - transform.position) * 0.75f;
-			transform.LookAt(player.transform.position);
-			audio.Play();
 
 			stage = 3;
 			break;
 		case 3:
-			this.gameObject.transform.position = transform.position + (player.transform.position - transform.position) * 0.85f;
-			transform.LookAt(player.transform.position);
-			audio.Play();
+			this.gameObject.transform.position = transform.position + (player.transform.position - transform.position);
 
 			break;
 		}
